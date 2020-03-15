c76faeab2e9713cb3d6300fb540d27991e31d2bb
Kim Sheng <pikacusheng@gmail.com>
2020-03-15 23:05:17 +0800

    Add baisc gameplay mechanic.
    Rearrange the scripts into scripts folder.
    
    Changes in MusicPlayerComponent
    - Add singleton.
    - Add Play a list of note function.
    
    Changes in GameFlowScript
    - Refactor few functions related to checking the answer.
    - Remove answering phase, instead each correct press will add the note to the answer list while the wrong one will clear the whoe answer list.

---
+++ Assets/Scripts/GameFlowScript.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using ProjectMusic;
+
+[System.Serializable]
+public struct MusicButton
+{
+    public int Key;
+    public Button Value;
+    public int limit;
+}
+
+public class GameFlowScript : MonoBehaviour
+{
+    // Button Setups
+    public Button BeginAnswerButton;
+    public Button PlayMusicButton;
+    public MusicButton[] MusicNoteButtons;
+    
+
+    // Solution Setups
+    public int[] Solution;
+    public int[] RandomizeKeyArray;
+
+    // Answers
+    public List<int> Answers;
+
+    private void Start()
+    {
+        MusicPlayerComponent.Instance.SetAudioList(Solution);
+
+        // Setup delegates
+        PlayMusicButton.onClick.AddListener(PlayMusic);
+        RandomArrayElement();
+
+        for (int i = 0; i < MusicNoteButtons.Length; ++i)
+        {
+            MusicButton CurrentButton = MusicNoteButtons[i];
+            CurrentButton.Key = RandomizeKeyArray[i];
+
+            CurrentButton.Value.onClick.AddListener(delegate { OnMusicNoteClicked(CurrentButton.Key); });
+        }
+        
+    }
+
+    void RandomArrayElement()
+    {
+        RandomizeKeyArray = (int[])Solution.Clone();
+        for (int index = 0; index < RandomizeKeyArray.Length; index++)
+        {
+            int temp = RandomizeKeyArray[index];
+            int randomIndex = Random.Range(0, RandomizeKeyArray.Length-1);
+            RandomizeKeyArray[index] = RandomizeKeyArray[randomIndex];
+            RandomizeKeyArray[randomIndex] = temp;
+        }
+    }
+
+    void OnMusicNoteClicked(int Key)
+    {
+        if (!MusicPlayerComponent.Instance.PlayAudioList)
+        {
+            MusicPlayerComponent.Instance.PlayMusicNote((NoteType)Key);
+            Answers.Add(Key);
+
+            if (CheckAnswer())
+            {
+                Debug.Log("Correct! Congratulations!");
+            }
+            else
+            {
+                Debug.Log("Incorrect Answer!");
+            }
+        }
+    }
+
+    public bool CheckAnswer()
+    {
+        for (int i = 0; i < Answers.Count; ++i)
+        {
+            int AnswerInt = Answers[i];
+            int SolutionInt = Solution[i];
+
+            if (AnswerInt != SolutionInt)
+            {
+                Answers.Clear();
+                break;
+            }
+        }
+        if (Solution.Length == Answers.Count)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void PlayMusic()
+    {
+        MusicPlayerComponent.Instance.InitiatePlayAudioList();
+    }
+}
---
+++ Assets/Scripts/MusicPlayerComponent.cs
@@ -0,0 +1,182 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ProjectMusic;
+
+[System.Serializable]
+public struct MusicNotePair
+{
+    public NoteType Key;
+    public AudioClip Value;
+
+    public MusicNotePair(NoteType Note)
+    {
+        Key = Note;
+        Value = null;
+    }
+
+    public MusicNotePair(int Note)
+    {
+        Key = (NoteType)Note;
+        Value = null;
+    }
+}
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicPlayerComponent : MonoBehaviour
+{
+    //Singleton
+    //!--------------------------------------------------
+    private static MusicPlayerComponent m_Instance;
+
+    public static MusicPlayerComponent Instance { get { return m_Instance; } }
+
+
+    /*private static bool m_ShuttingDown = false;
+    private static object m_Lock = new object();
+    private static MusicPlayerComponent m_Instance;
+
+    public static MusicPlayerComponent Instance
+    {
+        get
+        {
+            if (m_ShuttingDown)
+            {
+                return null;
+            }
+
+            lock (m_Lock)
+            {
+                if (m_Instance = null)
+                {
+
+                    m_Instance = Object.FindObjectOfType<MusicPlayerComponent>();
+
+                    if (m_Instance == null)
+                    {
+                        var singletonObject = new GameObject();
+                        m_Instance = singletonObject.AddComponent<MusicPlayerComponent>();
+                        singletonObject.name = "SoundManager (Singleton)";
+
+                        DontDestroyOnLoad(singletonObject);
+                    }
+                }
+                return m_Instance;
+            }
+        }
+        
+    }
+
+    private void OnApplicationQuit()
+    {
+        m_ShuttingDown = true;
+    }
+
+    private void OnDestory()
+    {
+        m_ShuttingDown = true;
+    }*/
+    //----------------------------------------
+    public AudioSource AudioSourceComp;
+    public bool PlayAudioList;
+    int Iterator;
+    float ClipTimer;
+    float CurrentClipLength;
+
+    public GameFlowScript GameFlow;
+    public int[] AudioList;
+
+    public MusicNotePair[] MusicNotes = new MusicNotePair[(int)NoteType.Count]
+    {
+        new MusicNotePair(0),
+        new MusicNotePair(1),
+        new MusicNotePair(2),
+        new MusicNotePair(3),
+        new MusicNotePair(4),
+        new MusicNotePair(5),
+        new MusicNotePair(6),
+        new MusicNotePair(7),
+        new MusicNotePair(8),
+        new MusicNotePair(9),
+        new MusicNotePair(10),
+        new MusicNotePair(11),
+        new MusicNotePair(12)
+    };
+
+    void Awake()
+    {
+        if (m_Instance != null && m_Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            m_Instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+
+        AudioSourceComp = GetComponent<AudioSource>();
+    }
+
+    void Start()
+    {
+        Iterator = 0;
+        ClipTimer = 0;
+        CurrentClipLength = 0;
+    }
+
+    void Update()
+    {
+        if (PlayAudioList)
+        {
+            LoopAudioList();
+        }
+    }
+
+    public void SetAudioList(int[] Sequence)
+    {
+        AudioList = Sequence;
+    }
+
+    public void PlayMusicNote(NoteType Note)
+    {
+        //Debug.Log(MusicNotes[(int)Note].Value);
+        AudioSourceComp.PlayOneShot(MusicNotes[(int)Note].Value);
+    }
+
+    public void InitiatePlayAudioList()
+    {
+        if (!PlayAudioList)
+        {
+            PlayAudioList = true;
+            Iterator = 0;
+            ClipTimer = 0;
+            AudioSourceComp.clip = MusicNotes[AudioList[Iterator]].Value;
+            CurrentClipLength = MusicNotes[AudioList[Iterator]].Value.length;
+            AudioSourceComp.Play();
+        }
+    }
+
+    void LoopAudioList()
+    {
+        if (Iterator < AudioList.Length - 1)
+        {
+            ClipTimer += Time.deltaTime;
+            if (ClipTimer >= CurrentClipLength)
+            {
+                ClipTimer = 0;
+                AudioSourceComp.Stop();
+
+                Iterator++;
+                
+                AudioSourceComp.clip = MusicNotes[AudioList[Iterator]].Value;
+                CurrentClipLength = MusicNotes[AudioList[Iterator]].Value.length;
+                AudioSourceComp.Play();
+            }
+        }
+        else
+        {
+            PlayAudioList = false;
+        }
+    }
+ }
